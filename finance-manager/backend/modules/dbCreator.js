a92cca3fb92115ecf09bc369214b90941c56f93f
IsmaelRK <ismael2021ab@gmail.com>
2024-02-16 15:52:17 -0300

    dbCreator.js improvements

--- finance-manager/backend/modules/dbCreator.js
+++ finance-manager/backend/modules/dbCreator.js
@@ -1,53 +1,35 @@
 const sqlite3 = require('sqlite3').verbose()
+const fs = require('fs')
 
-function initDatabase() {
-
-    const db = new sqlite3.Database('./database_sqlite')
+function buildDatabase(db) {
 
     db.serialize(() => {
         db.run('CREATE TABLE IF NOT EXISTS transactions (id INTEGER PRIMARY KEY AUTOINCREMENT, type TEXT, value REAL)')
-
         db.run('CREATE TABLE IF NOT EXISTS current_balance (id INTEGER PRIMARY KEY, subtotal REAL)')
-        db.get('SELECT * from current_balance', function (error, row) {
-            if (error) {
-                console.error("Error", error.message)
-            }
-            else {
-                if (!row) {
-                    db.run('INSERT INTO current_balance (subtotal) VALUES (0)', function (error) {
-                        if (error) {
-                            console.error("Error", error.message)
-                        }
-                        else {
-                            console.log("Success")
-                        }
-                    })
-                }
-            }
-        })
-
         db.run('CREATE TABLE IF NOT EXISTS balance (id INTEGER PRIMARY KEY, total REAL)')
-        db.get('SELECT * from balance', function (error, row) {
-
-            if (error) {
-                console.error("Error", error.message)
-            }
-            else {
-                if (!row) {
-                    db.run('INSERT INTO balance (total) VALUES (0)', function (error) {
-                        if (error) {
-                            console.error("Error", error.message)
-                        }
-                        else {
-                            console.log("Success")
-                        }
-                    })
-                }
-            }
-        })
+        db.run('INSERT INTO current_balance (subtotal) VALUES (0)')
+        db.run('INSERT INTO balance (total) VALUES (0)')
     })
 
-    return db
+}
+
+function initDatabase() {
+
+    const dbAlreadyCreated = fs.existsSync('./database_sqlite')
+    const db = new sqlite3.Database('./database_sqlite')
+
+    if (dbAlreadyCreated) {
+        return db
+    }
+
+    try {
+        buildDatabase(db)
+        console.log("Database successfully built! ")
+        return db
+    }
+    catch (error) {console.error("Error building database")}
+
+
 }
 
 module.exports = {initDatabase}
